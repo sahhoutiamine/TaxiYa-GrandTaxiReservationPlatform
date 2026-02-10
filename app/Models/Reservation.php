635c5af3e8aee976daa29c901e5aa6add76ccaf4
sahhoutiamine <midoriaduko@gmail.com>
2026-02-10 21:57:46 +0100

    Create Reservation model

---
+++ app/Models/Reservation.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'trip_id',
+        'user_id',
+        'seat',
+        'status',
+        'total_price',
+    ];
+
+    public function trip()
+    {
+        return $this->belongsTo(Trip::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function payment()
+    {
+        return $this->hasOne(Payment::class);
+    }
+}
