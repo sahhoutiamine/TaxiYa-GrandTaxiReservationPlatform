f5ac9e5922f90946c15d418b6bf064a41ecad18b
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-02-11 12:12:34 +0100

    add new route for testing

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', ['App\Http\Controllers\SearchController', 'index'])->name('home');
 Route::get('/search', ['App\Http\Controllers\SearchController', 'search'])->name('search');
+Route::get('/test', ['App\Http\Controllers\SearchController', 'search'])->name('search');
 
