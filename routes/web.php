4e2a751f628c3bbb79e79de209d22ccb8ccbfac3
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-02-12 12:48:50 +0100

    route the payment page with ReserveController that takes the trip id from the url

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SearchController;
-
+use \App\Http\Controllers\ReserveController;
 
 
 Route::get('/', [SearchController::class, 'index'])->name('home');
@@ -18,6 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('payment/{id}', [ReserveController::class, 'getSeats'])->name('payment');
 });
 
-require __DIR__.'/auth.php';
+
+require __DIR__ . '/auth.php';
