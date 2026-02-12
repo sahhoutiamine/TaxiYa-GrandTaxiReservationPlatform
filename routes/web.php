665091ae211b682a685d8dc0042434a86109d20d
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-02-12 15:41:04 +0100

    create a route to my bookings page

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,12 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::view('mybookings', 'traveler.mybookings')->name('mybookings');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('payment/{id}', [ReserveController::class, 'index'])->name('payment');
+    Route::post('payment/{id}', [ReserveController::class, 'store'])->name('payment.process');
 });
 
 
