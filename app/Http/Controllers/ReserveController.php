1807eb6e8de6c4220fc831e9386381b8bc0d6580
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-02-12 14:59:04 +0100

    the reserveController will also send the trip data to show a trip summary to the user

--- app/Http/Controllers/ReserveController.php
+++ app/Http/Controllers/ReserveController.php
@@ -4,18 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\Reservation;
+use App\Models\Trip;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class ReserveController extends Controller
 {
-    public function getSeats($id)
+    public function index($id)
     {
+        $trip = Trip::with(['departureCity', 'arrivalCity'])->findOrFail($id);
+
         $takenSeats = Reservation::where('trip_id', $id)
             ->where('status', 'confirmed')
             ->pluck('seat')
             ->toArray();
 
-        return view('traveler.confirm', compact('takenSeats'));
-    }
+        $deltaX = $trip->arrivalCity->x - $trip->departureCity->x;
+        $deltaY = $trip->arrivalCity->y - $trip->departureCity->y;
+        $distance = sqrt(pow($deltaX, 2) + pow($deltaY, 2)) * 100;
 
+        $durationHours = $distance / 60;
+        $departureDate = Carbon::parse($trip->departure_date);
+        $arrivalDate = $departureDate->copy()->addHours($durationHours);
+
+        return view('traveler.confirm', [
+            'trip' => $trip,
+            'takenSeats' => $takenSeats,
+            'distance' => round($distance),
+            'departureDate' => $departureDate,
+            'arrivalDate' => $arrivalDate
+        ]);
+    }
 }
