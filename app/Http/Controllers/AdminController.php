5427b8397b3bd961602a1771ecddddc6100df900
arabi lioubi <l.loubi1886@uca.ac.ma>
2026-02-13 12:23:00 +0100

    add rides function method to adminController to get trips data from trip model and redirect to rides page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,30 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Trip;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    
+
     public function dashboard()
     {
-       
+
     }
 
-   
+
     public function drivers()
     {
-       
+
     }
 
-   
+
     public function travelers()
     {
-       
+
     }
 
     public function rides()
     {
-       
+        $rides = Trip::with(['cheffeur', 'departureCity', 'arrivalCity'])
+            ->withSum(['reservations' => function ($query) {
+                $query->where('status', 'confirmed');
+            }], 'seat')
+            ->orderBy('departure_date', 'desc')
+            ->get();
+
+        return view('admin.rides', compact('rides'));
     }
 }
