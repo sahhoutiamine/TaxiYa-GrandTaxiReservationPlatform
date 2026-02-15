6fd24ea77d10ff0df45562ed4688e8c12e97b953
OUBELKACEM Ahmed <oubelkacemahmed78@gmail.com>
2026-02-15 13:17:03 +0000

    merge updates

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,10 +10,6 @@ use App\Models\Reservation;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
-use App\Models\User;
-use App\Models\Reservation;
-use Carbon\Carbon;
-
 class AdminController extends Controller
 {
 
